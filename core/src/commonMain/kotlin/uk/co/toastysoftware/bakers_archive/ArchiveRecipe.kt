88c81b3ee6858b6ee0de132edcf94e2711d91bb8
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-18 16:27:23 +0100

    Add(ArchiveRecipe): Add data class to represent an archive recipe based on the atom feed data.

---
+++ core/src/commonMain/kotlin/uk/co/toastysoftware/bakers_archive/ArchiveRecipe.kt
@@ -0,0 +1,14 @@
+package uk.co.toastysoftware.bakers_archive
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ArchiveRecipe(
+    val id: Long,
+    val name: String,
+    val url: String,
+    val summary: String,
+    val author: String,
+    val updated: String,
+    val archive: String
+)
